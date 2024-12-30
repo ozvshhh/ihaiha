f01c3571f3a0e15130984808cefb3dcba85346b0
ozvshhh <74505869+ozvshhh@users.noreply.github.com>
2024-12-30 11:07:57 +0000

    add : top bar 기본 구조 완성

--- ihaiha/src/components/topbar.tsx
+++ ihaiha/src/components/topbar.tsx
@@ -3,14 +3,14 @@ import menu from "../assets/icons/menu.svg"
 
 export const TopBar:React.FC= ()=>{
     return(
-        <div className="flex 	w-screen border-4 border-black ">
+        <div className="flex justify-between">
           <img src={menu} className="ml-7"/>
-          <div className="w-f flex flex-auto justify-center items-center h-ful">
-            <div className="flex-auto text-lg ">메인메뉴</div>
-            <div className="flex-auto text-lg">메인메뉴</div>
-            <div className="flex-auto text-lg">메인메뉴</div>
-            <div className="flex-auto text-lg">메인메뉴</div>
-            <div className="flex-auto text-lg">메인메뉴</div>
+          <div className="w-f flex flex-auto justify-center items-center mx-100">
+            <div className="flex text-lg mx-[50px] whitespace-nowrap">메인메뉴</div>
+            <div className="flex text-lg mx-[50px] whitespace-nowrap">메인메뉴</div>
+            <div className="flex text-lg mx-[50px] whitespace-nowrap">메인메뉴</div>
+            <div className="flex text-lg mx-[50px] whitespace-nowrap">메인메뉴</div>
+            <div className="flex text-lg mx-[50px] whitespace-nowrap">메인메뉴</div>
           </div>
         </div>
     )
